e31c0b38e9f0c1cc4cc01f0a0ab8495c6e74b7fd
Elias Sarband <elias.sarband@gmail.com>
2022-11-27 17:16:28 +0330

    Add project files.

---
+++ ChallengeCode/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ChallengeCode;
+using System.Net.Http.Headers;
+
+Solution sln = new Solution();
+
+int[] tokens = { 100 , 200 , 300 , 400};
+int score = sln.BagOfTokenScore(tokens, 200);
+
+Console.WriteLine(score);
+
+bool str = sln.isValid("([]){}");
+
+Console.WriteLine(str);
---
+++ ChallengeCode/Solution.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeCode
+{
+    public class Solution
+    {
+        static int finalScore = 0;
+        public int BagOfTokenScore(int[] tokens, int power)
+        {
+
+            for (int i = 0; i < tokens.Length; ++i)
+            {
+                List<int> tempToken = tokens.ToList<int>();
+                tempToken.RemoveAt(i);
+
+                RecursiveBag(i, tokens[i], tempToken.ToArray<int>(), power, 'U', "", 0);
+                RecursiveBag(i, tokens[i], tempToken.ToArray<int>(), power, 'D', "", 0);
+
+
+            }
+
+            return finalScore;
+        }
+
+        public void RecursiveBag(int tokenIndex,int tokenValue,int[] tokens, int power,char face, string path, int score)
+        {
+            if (face == 'U' && tokenValue <= power)
+            {
+                power -= tokenValue;
+                ++score;
+                path += tokenIndex.ToString() + face;
+            }
+
+            if (face == 'D' && score > 0)
+            {
+                power += tokenValue;
+                --score;
+                path += tokenIndex.ToString() + face;
+            }
+
+            if (tokens.Length > 0)
+            {
+                
+                for (int i = 0; i < tokens.Length; ++i)
+                {
+                    List<int> tempToken = tokens.ToList<int>();
+                    tempToken.RemoveAt(i);
+
+                    RecursiveBag(i, tokens[i], tempToken.ToArray<int>(), power, 'U', "", score);
+                    RecursiveBag(i, tokens[i], tempToken.ToArray<int>(), power, 'D', "", score);
+                }
+            }
+            else
+            {
+                if (score > finalScore)
+                {
+                    finalScore = score;
+                }
+            }
+        }
+
+        public bool isValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            char[] str = s.ToCharArray();
+
+            for (int i = 0; i < str.Length; ++i)
+            {
+                if (str[i]== '(' || str[i] == '[' || str[i] == '{')
+                {
+                    stack.Push(str[i]);
+                }
+                else if (str[i] == ')')
+                {
+                    if (stack.Pop() != '(')
+                    {
+                        return false;
+                    }
+                }
+                else if (str[i] == ']')
+                {
+                    if (stack.Pop() != '[')
+                    {
+                        return false;
+                    }
+                }
+                else if (str[i] == '}')
+                {
+                    if (stack.Pop() != '{')
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
